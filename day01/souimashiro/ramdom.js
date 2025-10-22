8f7107cc451c68e8fa18dd250fe50a1c32844beb
今城　颯 <sajincheng6@gmail.com>
2025-10-23 06:17:54 +0900

    day1未完了

---
+++ day01/souimashiro/ramdom.js
@@ -0,0 +1,9 @@
+const alphabet = "abcdefghijklmnopqrstuwxyz".split("");
+
+function getRamdomIndex() {
+    return Math.floor(Math.random() * alphabet.length);
+}
+
+function printRamdomAlphabet() {
+    let result = "";
+}
