36e366e4ff54ea261e40d6dcb4ddebd23c545c03
今城　颯 <sajincheng6@gmail.com>
2025-11-04 07:34:45 +0900

    day04完了

---
+++ day04/imashiro/index.js
@@ -0,0 +1,12 @@
+const input = "a a b c c";
+
+let found = -1;
+
+for(let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]){
+        found = arr[i];
+        break;
+    }
+}
+
+console.log(found);
