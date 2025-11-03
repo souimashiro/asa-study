9a1a0a0ae9dcf35a0ea31c44439314153d8a8119
shoki0322 <shou.k.0322@icloud.com>
2025-11-04 07:37:52 +0900

    day04修正

--- day04/kannno/index.js
+++ day04/kannno/index.js
@@ -1,26 +1,17 @@
-
-
-
-
 const testFunc = (s) => {
 
     const str = s.split(/[ ,;]+/);
 
 
-for (let i = 1; i < str.length; i++) {
-  if (str[i] === str[i - 1]) {
-    return str[i]
-  } else {
+    for (let i = 1; i < str.length; i++) {
+        if (str[i] === str[i - 1]) {
+            return str[i]
+        } 
+    }
     return -1
-  }
-}
-
 }
 
 
-
-
-
-console.log(testFunc("a a b a a"))
+console.log(testFunc("a b b a a"))
 console.log(testFunc("x y z"))
 console.log(testFunc("m m m n n"))
